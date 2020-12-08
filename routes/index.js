936a2e50e06d044b653b98dc50a81542efab8d31
Lexie White <lexiewhite@Lexies-MacBook-Air.local>
2020-12-08 10:21:09 -0600

    complete part three

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    user: "Lexie",
+    text: "I'm making an express server from scratch",
+    dateCreated: new Date()
+  },
+  {
+    user: "Claire",
+    text: "That's awesome!",
+    dateCreated: new Date()
+  },
+  {
+    user: "Anna",
+    text: "I wish I could do that!",
+    dateCreated: new Date()
+  }
+]
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
