bdfdb9f74a8301f4770762276b55f0934d7e50a2
Lexie White <lexiewhite@Lexies-MacBook-Air.local>
2020-12-09 08:58:17 -0600

    part five completed

--- routes/new.js
+++ routes/new.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('form')
 });
+
+// TODO: create post request
+//router.post('/')
 
 module.exports = router;
